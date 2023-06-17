8dbcaaefbbc63f15b2bbcd90bb92e4df423980b8
Adikwu Samuel <Adikwu.S@bullseye.local>
2023-06-17 15:55:03 +0100

    Pricing component toggle master

--- pricing component/priceList.js
+++ pricing component/priceList.js
@@ -1,14 +1,15 @@
-const switchToggle = document.getElementById('switchToggle')
-const nuts = document.getElementById('nuts')
-const monthly = document.getElementsByClassName('#month')
-const annually = document.getElementsByClassName('#annual')
 
-const price = document.getElementsByName("price")
+const switchToggle = document.getElementById("switchToggle");
+      const nuts = document.getElementById("nuts");
+      const monthly = document.getElementsByClassName("show-price");
+      const annually = document.getElementsByClassName("hide-price");
 
-
-switchToggle.addEventListener('click', ()=>{
-    nuts.classList.toggle("toggle")
-    annually.classList.toggle("show-price")
-    monthly.classList.toggle("hide-price")
-    // price.classList.toggle("show-price")
-})
+      switchToggle.addEventListener("click", () => {
+        nuts.classList.toggle("toggle");
+        for (let i = 0; i < annually.length; i++) {
+          annually[i].classList.toggle("show-price");
+        }
+        for (let i = 0; i < monthly.length; i++) {
+          monthly[i].classList.toggle("hide-price");
+        }
+      });
